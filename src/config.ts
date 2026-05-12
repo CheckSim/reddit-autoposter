28a8c1bdf8fe6628cffefd3c6a969d3c70b0461a
CheckSim <smn.ccc@gmail.com>
2026-05-12 08:49:41 +0200

    template final

--- src/config.ts
+++ src/config.ts
@@ -3,14 +3,14 @@
 
 export const APP_CONFIG = {
   // Nome dell'app (deve corrispondere a "name" in devvit.json)
-  appName: 'rtaweeklyposter',
+  appName: 'MY_APP_NAME',
 
   // Cron schedule
   // Esempi:
   //   '0 9 * * 1'      → ogni lunedì alle 09:00 UTC
   //   '0 9 * * 1,3,5'  → lunedì, mercoledì, venerdì alle 09:00 UTC
   //   '0 9 1 * *'      → il primo del mese alle 09:00 UTC
-  cron: '0 9 * * 1',
+  cron: '*/5 * * * *', // Ogni 5 minuti (per test, cambia in '0 9 * * 1' per produzione)
 
   // Titolo del post
   postTitle: 'Discussione Settimanale 🗓️',
